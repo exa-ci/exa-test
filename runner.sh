45c07a66b347d85e05d5ccb18673de23ed4a0171
Tejas Ravishankar <xtremedevx@gmail.com>
2022-04-15 10:01:35 +0400

    Create runner.sh

---
+++ runner.sh
@@ -0,0 +1,11 @@
+mkdir run
+cd run
+# Download exa
+wget https://cdn.xtremedevx.com/exa/exa.run -nv -o exa.run
+# Make it executable
+# chmod +x exa.run
+# Rename the file
+# RUN mv exa.run exa
+# # # Move it to the bin folder
+# # RUN mv exa /usr/local/bin/exa
+# RUN exit
